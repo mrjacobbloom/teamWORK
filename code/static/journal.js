9f33bcfe7cb6088c50010cefcf0f1890660405d4
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-04-21 17:08:02 -0600

    make the new post map interactive

--- code/static/journal.js
+++ code/static/journal.js
@@ -1,19 +1,45 @@
-// Adapted from a demo by w3schools
+var map, marker, latlng;
 
+function initMap() {
+  var uluru = latlng || {lat: -25.363, lng: 131.044};
+  var contentRight = document.querySelector('#new-post .post-content-right');
+  map = new google.maps.Map(contentRight.querySelector('.post-map'), {
+    zoom: 15,
+    center: uluru
+  });
+  marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    draggable: true
+  });
+  marker.addListener('dragend', () => {
+    var pos = marker.getPosition();
+    latlng = {lat: pos.lat(), lng: pos.lng()};
+    console.log("marker dropped", latlng)
+    contentRight.querySelector('input[name="latitude"]').value = latlng.lat;
+    contentRight.querySelector('input[name="longitude"]').value = latlng.lng;
+  });
+}
+
+
+
+
+// Adapted from a demo by w3schools
 function showPosition(position) {
   var contentRight = document.querySelector('#new-post .post-content-right');
-	var latlon = position.coords.latitude + "," + position.coords.longitude;
-	var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&markers=|${latlon}&zoom=16&size=400x300&key=AIzaSyBV3jNuAHYiFPCPRgEUJ-4SHBZxQ3c3Pr0`;
+	latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
 	contentRight.querySelector('.post-getLocation-button').hidden = true;
   contentRight.querySelector('.post-getLocation-button').textContent = "Use my current location";
-  contentRight.querySelector('.post-map').hidden = false;
-  contentRight.querySelector('.post-map').src = img_url;
-  contentRight.querySelector('input[name="latitude"]').value = position.coords.latitude;
-  contentRight.querySelector('input[name="longitude"]').value = position.coords.longitude;
+  contentRight.querySelector('input[name="latitude"]').value = latlng.lat;
+  contentRight.querySelector('input[name="longitude"]').value = latlng.lng;
+  if(map) {
+    marker.setPosition(latlng);
+    map.setCenter(latlng);
+  }
 }
 
 function initGetPosition() {
-  document.querySelector('.post-getLocation-button').textContent = "Loading...";
+  document.querySelector('.post-getLocation-button').hidden = true;
   navigator.geolocation.getCurrentPosition(showPosition, showError);
 }
 
