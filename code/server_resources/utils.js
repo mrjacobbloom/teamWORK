07ad45edb747b010c0df6759b36f3ab8b1256f05
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-04-23 00:48:54 -0600

    change database to store post datetime instead of just date

--- code/server_resources/utils.js
+++ code/server_resources/utils.js
@@ -12,34 +12,35 @@ module.exports = {
   timeSince: function(date) { // https://stackoverflow.com/a/3177838/1784306
     var seconds = Math.floor((new Date() - date) / 1000);
     var interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {
-      return interval + " years";
+    if (interval >= 1) {
+      return interval + " year" + ((interval != 1) ? "s" : "") + " ago";
     }
     interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-      return interval + " months";
+    if (interval >= 1) {
+      return interval + " month" + ((interval != 1) ? "s" : "") + " ago";
     }
     interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-      return interval + " days";
+    if (interval >= 1) {
+      return interval + " day" + ((interval != 1) ? "s" : "") + " ago";
     }
     interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-      return interval + " hours";
+    if (interval >= 1) {
+      return interval + " hour" + ((interval != 1) ? "s" : "") + " ago";
     }
     interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-      return interval + " minutes";
+    if (interval >= 1) {
+      return interval + " minute" + ((interval != 1) ? "s" : "") + " ago";
     }
-    return Math.floor(seconds) + " seconds";
+    interval = Math.floor(seconds);
+    return interval + " second" + ((interval != 1) ? "s" : "") + " ago";
   },
   getCurrentDateString: function() {
-    function padLeft(str, len) {
+    function padLeft(str, len = 2) {
       str = str.toString();
       return "0".repeat(Math.min(0, len - str.len)) + str;
     }
     var now = new Date();
-    return `${now.getFullYear()}-${padLeft(now.getMonth() + 1)}-${padLeft(now.getDate())}`;
+    return `${now.getFullYear()}-${padLeft(now.getMonth() + 1)}-${padLeft(now.getDate())} ${padLeft(now.getHours())}:${padLeft(now.getMinutes())}:${padLeft(now.getSeconds())}`;
   },
   passErrors: function(path, errors) {
     if(!Array.isArray(errors)) errors = [errors];
