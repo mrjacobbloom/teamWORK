759696a053557e56eb607ae786225e2e36b7cb5d
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-04-25 00:22:08 -0600

    update db script so test posts follow new format

--- code/sql/db.sql
+++ code/sql/db.sql
@@ -31,9 +31,9 @@ create table data (
     
 );
 
-insert into data values ('robert','I saw a squirrel','the squirrel was in my yard',40.0340975,-105.27215699999,'2018-04-04 12:00:00');
-insert into data values ('robert','I saw another squirrel','This cheeky squirrel was in a tree.',40.0340975,-105.27215699999,'2018-04-05 12:00:00');
-insert into data values ('john','Spotted a buffalo near I-95','the buffalo was standing by the highway',41.0340975,-107.27215699999,'2018-04-04 12:00:00');
-insert into data values ('jane','Spider in my shower!','Went to take a shower and found a spider hanging out in my shower.',40.0340975,-105.27215699999,'2018-04-01 12:00:00');
-insert into data values ('jane','Marmot in my sink!','Went to wash my hands and spotted a marmot in my sink.',40.0340975,-105.27215699999,'2018-04-01 12:00:00');
-insert into data values ('Ulfric','Deer in the road','Driving to whiterun and a deer crossed my path in the road.',42.000,-2.27215699999,'2018-01-01 12:00:00');
+insert into data values ('robert','squirrel','the squirrel was in my yard',40.0340975,-105.27215699999,'2018-04-04 12:00:00');
+insert into data values ('robert','another squirrel','This cheeky squirrel was in a tree.',40.0340975,-105.27215699999,'2018-04-05 12:00:00');
+insert into data values ('john','buffalo near I-95','the buffalo was standing by the highway',41.0340975,-107.27215699999,'2018-04-04 12:00:00');
+insert into data values ('jane','spider in my shower!','Went to take a shower and found a spider hanging out in my shower.',40.0340975,-105.27215699999,'2018-04-01 12:00:00');
+insert into data values ('jane','marmot in my sink!','Went to wash my hands and spotted a marmot in my sink.',40.0340975,-105.27215699999,'2018-04-01 12:00:00');
+insert into data values ('Ulfric','deer in the road','Driving to whiterun and a deer crossed my path in the road.',42.000,-2.27215699999,'2018-01-01 12:00:00');
